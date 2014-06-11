79a6b191b63151fdd05129f61d43ba9ebd13ebe8
Tobias Bleckert <tbleckert@gmail.com>
2014-06-11 09:09:51 +0200

    Strict mode

--- Router.js
+++ Router.js
@@ -8,6 +8,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 (function() {
 
+	'use strict';
+	
 	window.Router = (function() {
 	
 		Router.namedParam = /:\w+/g;
